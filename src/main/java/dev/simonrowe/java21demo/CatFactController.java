ffadd02c1905aebe91c9d984cec3ff69ce79fd41
Simon Rowe <simon.rowe@global.com>
2024-01-22 21:53:09 +0000

    feat: add in Faker api and jmeter tests as well as spring dev tools

--- src/main/java/dev/simonrowe/java21demo/CatFactController.java
+++ src/main/java/dev/simonrowe/java21demo/CatFactController.java
@@ -1,6 +1,8 @@
 package dev.simonrowe.java21demo;
 
 
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class CatFactController {
 
     private final CatFactClient catFactClient;
+    private final Faker faker = new Faker();
 
     public CatFactController(CatFactClient catFactClient) {
         this.catFactClient = catFactClient;
     }
 
-    @GetMapping("/catfact")
-    CatFact catFact() {
+    @GetMapping("/catfact/proxy")
+    CatFact catFactProxy() {
         return catFactClient.randomFact();
     }
+
+    @GetMapping("/catfact/faker")
+    @SneakyThrows
+    CatFact catFactFaker() {
+        Thread.sleep(300);
+        var catfact = faker.cat().name();
+        return new CatFact(catfact, catfact.length());
+    }
+
 
 }
