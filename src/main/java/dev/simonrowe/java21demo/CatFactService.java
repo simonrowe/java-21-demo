c9396278560733be9ceb6bd53a89b9dc4ac58f5b
Simon Rowe <simon.rowe@global.com>
2024-01-23 06:40:57 +0000

    feat: add distributed tracing

--- src/main/java/dev/simonrowe/java21demo/CatFactService.java
+++ src/main/java/dev/simonrowe/java21demo/CatFactService.java
@@ -2,10 +2,12 @@ package dev.simonrowe.java21demo;
 
 import com.github.javafaker.Faker;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CatFactService {
 
     private final Faker faker = new Faker();
@@ -15,6 +17,7 @@ public class CatFactService {
     public CatFact getCatFact() throws InterruptedException {
         Thread.sleep(300);
         var catfact = String.format("%s: %s", faker.cat().name(), faker.chuckNorris().fact());
+        log.info("Retrieved catfact: {}", catfact);
         return new CatFact(catfact, catfact.length());
     }
 }
